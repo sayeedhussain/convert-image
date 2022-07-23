5b1821f0599c74b77f9752682306f85896173116
Sayeed <sayeedhussain19@gmail.com>
2022-07-23 23:34:40 +0530

    Formula

--- convert-image.rb
+++ convert-image.rb
@@ -1,8 +1,8 @@
 class ConvertImage < Formula
-  desc "Convert image from heic to jpeg"
+  desc "Convert image from heic to jpeg format"
   homepage ""
-  url "https://github.com/sayeedhussain/convert-image/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "5db1225505e52461b40c5a984e424a41b21f1c8d8c9dae87bf8f55caa88d65a4"
+  url "https://github.com/sayeedhussain/convert-image/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "20852b173909d7f70a52f9ad7a0c21e807efc92499d5b30dceca50018a67d1b6"
   license "GPL-3.0"
  
   def install
