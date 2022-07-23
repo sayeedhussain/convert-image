89514b3ec3eafe97c26f274d4bd8c581a57cc297
Sayeed <sayeedhussain19@gmail.com>
2022-07-24 00:55:22 +0530

    Update formula with bash dependency

--- convert-image.rb
+++ convert-image.rb
@@ -4,6 +4,8 @@ class ConvertImage < Formula
   url "https://github.com/sayeedhussain/convert-image/archive/refs/tags/v0.0.6.tar.gz"
   sha256 "4d8f567d290b6bcd48e58a2c308e2b09817b2eb134336a5d687fed581105a5f3"
   license "GPL-3.0"
+
+  depends_on "bash" => :run
  
   def install
     bin.install "convert_heic_to_jpeg"
