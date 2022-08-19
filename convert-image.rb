09d78ac150d876ed50510b8f88b258f619b2318b
Sayeed <sayeedhussain19@gmail.com>
2022-08-19 16:04:14 +0530

    Fix error when filename or images folder has white spaces

--- convert-image.rb
+++ convert-image.rb
@@ -1,8 +1,8 @@
 class ConvertImage < Formula
   desc "Convert image from heic to jpeg format"
   homepage ""
-  url "https://github.com/sayeedhussain/convert-image/archive/refs/tags/v0.0.6.tar.gz"
-  sha256 "4d8f567d290b6bcd48e58a2c308e2b09817b2eb134336a5d687fed581105a5f3"
+  url "https://github.com/sayeedhussain/convert-image/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "c62cf7ae6a94e55dcfdc8c00f34caccec1f2e9bad7b0f6ea5e6e0578d56ecc4a"
   license "GPL-3.0"
 
   depends_on "bash" => :run
